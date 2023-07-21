53d97b1ce92fd29c6f648c9db87a06a920ab3951
GLCVLL <g.ioele96@outlook.it>
2023-07-21 15:00:48 +0200

    added constructor

--- index.php
+++ index.php
@@ -5,4 +5,11 @@ class Movie {
     public $title;
     public $genre;
     public $releaseYear;
+
+    // Costruttore della classe
+    public function __construct($title, $genre, $releaseYear) {
+        $this->title = $title;
+        $this->genre = $genre;
+        $this->releaseYear = $releaseYear;
+    }
 }
