56c1fb9d8c44c7414ed272f5452498976f9a76f6
GLCVLL <g.ioele96@outlook.it>
2023-07-21 15:26:59 +0200

    added methiod

--- index.php
+++ index.php
@@ -13,8 +13,16 @@ class Movie {
         $this->releaseYear = $releaseYear;
     }
 
+    // Metodo d'istanza
+    public function getInfo() {
+        return "Titolo: " . $this->title . ", Genere: " . $this->genre . ", Anno di uscita: " . $this->releaseYear;
+    }
 }
 
 // Creazione di due oggetti Movie
 $movie1 = new Movie("Inception", "Fantascienza", 2010);
 $movie2 = new Movie("The Shawshank Redemption", "Drammatico", 1994);
+
+// Stampa delle informazioni relative ai due film
+echo "Film 1: " . $movie1->getInfo() . "<br>";
+echo "Film 2: " . $movie2->getInfo() . "<br>";
